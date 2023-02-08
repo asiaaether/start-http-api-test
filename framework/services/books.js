f30f11dd024287c90357615ea96c1d64f75cf088
Aliyah Khisamova <asia9520@gmail.com>
2023-02-08 11:51:04 +0300

    add book tests

--- framework/services/books.js
+++ framework/services/books.js
@@ -1,7 +1,9 @@
 import supertest from "supertest";
 import config from "../config/userConfig";
+import user, { token, userID } from "./user";
 const {url} = config;
 
+let ISBN = ''
 
 const book = {
 
@@ -26,6 +28,36 @@ const book = {
           ]
         })
     },
+
+    updateBook: (ISBN, token, userID) => {
+      return supertest(config.url)
+      .put(`/BookStore/v1/Books/${ISBN}`)
+      .set('Accept', 'application/json')
+      .set('Content-Type', 'application/json')
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        "userId": `${userID}`,
+        "isbn": '9781449331818'
+      })
+    },
+
+    getBookInfo: (ISBN) => {
+      return supertest(config.url)
+      .get(`/BookStore/v1/Book?ISBN=${ISBN}`)
+      .set('Accept', 'application/json')
+      .send()
+    },
+
+    deleteBook: (token, userID) => {
+      return supertest(config.url)
+      .del('/BookStore/v1/Book')
+      .set('Accept', 'application/json')
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        "isbn": "9781449331818",
+        "userId": `${userID}`
+      })
+    }
 
    
 }
